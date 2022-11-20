32b8fe44d9e6f4974e31127aac90e2ca86621663
ctYurk15 <levgenetic@gmail.com>
2022-11-20 12:25:48 +0200

    Add ship movement & asteroids creation

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public float shipMargin = 0.6f;
+    public float shipSpeed = 0.5f;
+
+    private float topEdge;
+    private float bottomEdge;
+
+    void Start()
+    {
+        bottomEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).y + shipMargin;
+        topEdge = -1 * bottomEdge;
+       // Debug.Log(topEdge + " " + bottomEdge);
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow) && transform.position.y <= topEdge)
+        {
+            transform.position += new Vector3(0, shipSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) && transform.position.y >= bottomEdge)
+        {
+            transform.position += new Vector3(0, -1 * shipSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] positions;
+    public GameObject asteroidPrefab;
+    public int spawnChance = 3;
+
+    public float interval = 1;
+
+
+    private void OnEnable()
+    {
+        InvokeRepeating(nameof(Spawn), interval, interval);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke(nameof(Spawn));
+    }
+
+    private void Spawn()
+    {
+        for(int i = 0; i < positions.Length; i++)
+        {
+            if(Random.Range(0, spawnChance) == 0)
+            {
+                GameObject asteroid = Instantiate(asteroidPrefab, positions[i].transform.position, Quaternion.identity);
+            }
+        }
+    }
+}
